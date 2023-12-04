68ac58bd0fed3f1079a225bffffb8ae3dd739318
mat <mat@northwestern.edu>
2023-12-03 22:14:01 -0500

    Initial commit

---
+++ canopy.ts
@@ -0,0 +1,30 @@
+require("dotenv").config();
+const buildConfig = require("./src/lib/build/config");
+const aggregate = require("./src/lib/build/aggregate");
+
+const args = process.argv;
+
+(() => {
+  const path = args
+    .find((value) => value.includes("--path="))
+    ?.split("=")
+    ?.pop();
+
+  const isDev = args.includes("dev");
+  const config = buildConfig.getConfig(path, isDev);
+
+  const url = isDev ? `http://localhost:5001` : process.env.NEXT_PUBLIC_URL;
+  const basePath = isDev ? `` : process.env.NEXT_PUBLIC_BASE_PATH;
+  const baseUrl = basePath ? `${url}${basePath}` : url;
+
+  const env = {
+    CANOPY_CONFIG: {
+      ...config,
+      url,
+      basePath,
+      baseUrl,
+    },
+  };
+
+  aggregate.build(env.CANOPY_CONFIG);
+})();
---
+++ next.config.js
@@ -0,0 +1,41 @@
+const { PHASE_DEVELOPMENT_SERVER } = require("next/constants");
+
+const { getConfig } = require("./src/lib/build/config.ts");
+
+module.exports = (phase) => {
+  const isDev = phase === PHASE_DEVELOPMENT_SERVER;
+  const config = getConfig(undefined, isDev);
+
+  const url = isDev ? `http://localhost:5001` : process.env.NEXT_PUBLIC_URL;
+  const basePath = isDev ? `` : process.env.NEXT_PUBLIC_BASE_PATH;
+  const baseUrl = basePath ? `${url}${basePath}` : url;
+
+  const env = {
+    CANOPY_CONFIG: {
+      ...config,
+      url,
+      assetPrefix: basePath,
+      basePath,
+      baseUrl,
+    },
+  };
+
+  const redirects = () => {
+    return [
+      {
+        source: "/works",
+        destination: "/search",
+        permanent: true,
+      },
+    ];
+  };
+
+  return {
+    env,
+    pageExtensions: ["ts", "tsx", "js", "jsx", "md", "mdx"],
+    redirects,
+    typescript: {
+      ignoreBuildErrors: true,
+    },
+  };
+};
